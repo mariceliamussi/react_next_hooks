6e1c47bfb53637e00dae7465f0558f831a060aeb
Mari <maricelia.mussi@gmail.com>
2023-06-22 19:33:13 -0300

    02_hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react"
 
 import './components/UseState_01'
-import HookState1 from "./components/UseState_01"
+// import HookState1 from "./components/UseState_01"
+import HookState2 from "./components/UseState_02"
 
 export default () => {
     return(
         <>
             <h1>REACT HOOKS</h1>
-            <HookState1 />
+            {/* <HookState1 /> */}
+            <HookState2 />
         </>
     )
 }
